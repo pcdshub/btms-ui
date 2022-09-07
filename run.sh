9588158b527c6440e181a03a024c60fcfbd074d1
Ken Lauer <klauer@slac.stanford.edu>
2022-09-07 16:16:03 -0700

    ENH: add quick run.sh until it gets into the environment

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source /reg/g/pcds/engineering_tools/latest-released/scripts/dev_conda ""
+
+cd $SCRIPT_DIR
+
+python -m btms_ui.bin.main "$@"
