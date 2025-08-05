f5000bdb27c99c967f4341aa754758e62b3a8c8f
aberges-SLAC <aberges@slac.stanford.edu>
2025-08-05 16:13:43 -0700

    ENH: Move away from dev_conda and freeze the latest working environment

--- btps.sh
+++ btps.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+export PCDS_CONVA_VER=6.0.1
+
 source /reg/g/pcds/engineering_tools/latest-released/scripts/pcds_conda ""
 
 # cd $SCRIPT_DIR
