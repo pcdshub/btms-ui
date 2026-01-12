b60c3b500880f3c346e7c03918d52e8964f1b2e6
aberges-SLAC <aberges@slac.stanford.edu>
2026-01-12 13:24:39 -0800

    BUG: fix typos

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export PCDS_CONDA_VER=6.0.1
+
 source /reg/g/pcds/engineering_tools/latest-released/scripts/pcds_conda ""
 
 cd $SCRIPT_DIR
